3f25960d3777ec8cbb3464a9d6e1bf3ce939b9d3
RodionovNikita <radzivonaumikita@gmail.com>
2026-06-02 14:50:07 +0200

    added Util class

---
+++ src/main/java/com/javarush/util/Util.java
@@ -0,0 +1,8 @@
+package com.javarush.util;
+
+public class Util {
+    public static final String DB_USER = "";
+    public static final String DB_PASSWORD = "";
+
+    public static final String CHANGE_LOG_PATH = "db/changelog/master.yaml";
+}
